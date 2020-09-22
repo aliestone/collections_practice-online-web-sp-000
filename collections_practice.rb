34def9f106d9a6c17d7d1416bfb553b1d7b2db40
Alie Stone <helloaliestone@gmail.com>
2020-09-22 16:47:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort{|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  array = [0,1,2]
+  array.swp [1,2]
+end
+
+def reverse_array(array)
+array.reverse
+end
+
+def find_a(array)
+  array.select { |word| word.start_with?('a') }
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
